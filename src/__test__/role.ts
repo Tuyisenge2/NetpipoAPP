9e5e534d5f7c826dc7b26d231d14d3717c162be6
Tuyisenge2 <tuyisengetito3@gmail.com>
2025-04-24 19:52:48 +0200

    ft new migration

---
+++ src/__test__/role.ts
@@ -0,0 +1,30 @@
+import { DataTypes, Sequelize, Model, UUIDV4 } from "sequelize";
+import database_models from "../config/db.config";
+import { roleCreationAttributes, roleModelAttributes } from "../../types/model";
+
+export class Role extends Model<roleModelAttributes, roleCreationAttributes> {
+	public static associate() {
+	}
+}
+const Role_model = (sequelize: Sequelize) => {
+	Role.init(
+		{
+			id: {
+				type: DataTypes.UUID,
+				defaultValue: UUIDV4,
+				primaryKey: true,
+				allowNull: false,
+			},
+			roleName: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+		},
+		{
+			sequelize,
+			tableName: "roles",
+		},
+	);
+	return Role;
+};
+export default Role_model;
